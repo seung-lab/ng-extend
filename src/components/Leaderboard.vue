f8da0b2e06eda5b0738d9b01ed3e2032f6b2f185
Kai Kuehner <kvk@pobox.com>
2020-02-05 18:18:09 -0500

    add leaderboard overlay

---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="nge-leaderboard">
+    <overlay class="modal overlay-content">
+      <div class="title">Top Editors This Week</div>
+      <!--
+        TODO:
+        this should display each leaderboard entry in a grid
+      -->
+    </overlay>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import Overlay from "components/Overlay.vue";
+
+export default Vue.extend({
+  components: { Overlay }
+});
+</script>
+
+<style>
+.nge-leaderboard {
+  position: fixed;
+  left: 10%;
+  top: 30%;
+  width: 10%;
+  height: 40%;
+  z-index: 50;
+}
+
+.nge-overlay-content {
+  min-width: 250px;
+}
+
+.nge-overlay-content .title {
+  background-color: lightgray;
+  font-size: 1.25em;
+}
+</style>
