698622dbbceaf62811269dff2b937c81cf30c6c3
Kai Kuehner <kvk@pobox.com>
2020-02-10 18:04:09 -0500

    add basic chat panel

---
+++ src/components/Chatbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nge-chatbox">
+    <div>Chat</div>
+    <form @submit.prevent="appState.submitMessage" autocomplete="off">
+      <input type="text" id="chatMessage">
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import {storeProxy} from "../state";
+
+export default Vue.extend({
+  data: () => {
+    return {
+      appState: storeProxy
+    }
+  }
+});
+</script>
+
+<style>
+.nge-chatbox {
+  background-color: #92af7d;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="nge-sidebar">
+    <div class="nge-sidebar-hidden" v-show="!visible">
+      <button class="nge-sidebar-button show" title="Show Sidebar" @click="setVisible(true);">↓</button>
+    </div>
+    <div class="nge-sidebar-visible" v-show="visible">
+      <button class="nge-leaderboard-button" title="Hide Sidebar" @click="setVisible(false);">↑</button>
+      <leaderboard />
+      <chatbox />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import {storeProxy} from "../state";
+import Leaderboard from "components/Leaderboard.vue";
+import Chatbox from "components/Chatbox.vue";
+
+export default Vue.extend({
+  components: { Leaderboard, Chatbox },
+  data: () => {
+    return {
+      appState: storeProxy,
+      visible: true
+    }
+  },
+  methods: {
+    setVisible: function(visible: boolean) {
+      this.visible = visible;
+    }
+  }
+});
+</script>
+
+<style>
+.nge-sidebar-visible {
+  width: 250px;
+  display: grid;
+  grid-template-rows: min-content auto auto;
+  height: 100%;
+}
+
+.nge-sidebar-button {
+  background-color: #222;
+  padding: 5px;
+}
+</style>
