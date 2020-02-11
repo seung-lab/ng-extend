e06b41743ecc3130d0e98224493b8f6ebe133f25
Kai Kuehner <kvk@pobox.com>
2020-02-11 14:28:42 -0500

    chat improvements

--- src/components/Chatbox.vue
+++ src/components/Chatbox.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="nge-chatbox">
-    <div class="nge-chatbox-message" v-for="(message, index) of appState.chatMessages" :key="index">
-      <div class="nge-chatbox-message-sender">{{message.name}}</div>
-      <div class="nge-chatbox-message-content">{{message.message}}</div>
+    <div class="nge-chatbox-title">Chat</div>
+    <div class="nge-chatbox-filler"></div>
+    <div class="nge-chatbox-messages">
+      <div class="nge-chatbox-message" v-for="(message, index) of appState.chatMessages" :key="index">
+        <div class="nge-chatbox-message-info">
+          <div class="nge-chatbox-message-sender">{{message.name}}</div>
+          <div class="nge-chatbox-message-time">{{message.time}}</div>
+        </div>
+        <div class="nge-chatbox-message-content">{{message.message}}</div>
+      </div>
     </div>
-    <form @submit.prevent="submitMessage" autocomplete="off">
+    <form class="nge-chatbox-sendmessage" @submit.prevent="submitMessage" autocomplete="off">
       <input type="text" id="chatMessage" />
-      <input type="submit" value="Submit" />
+      <button type="submit">Submit</button>
     </form>
   </div>
 </template>
@@ -33,7 +40,6 @@ export default Vue.extend({
       const name = this.appState.loggedInUser ? this.appState.loggedInUser.name : "Guest";
 
       const messageObj = { name: name, message: message };
-      console.log("sending message", messageObj);
       ws.send(JSON.stringify(messageObj));
     }
   }
@@ -42,6 +48,51 @@ export default Vue.extend({
 
 <style>
 .nge-chatbox {
-  background-color: #92af7d;
+  display: grid;
+  grid-template-rows: min-content auto minmax(auto, min-content) min-content;
+  background-color: #111;
 }
+
+.nge-chatbox-title {
+  background-color: #333;
+  font-size: 1.25em;
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+  text-align: center;
+}
+
+.nge-chatbox-messages {
+  overflow: auto;
+  overflow-wrap: break-word;
+  font-size: 0.85em;
+}
+
+.nge-chatbox-message-info {
+  background-color: #222;
+  padding: 0.5em;
+  display: grid;
+  grid-template-columns: minmax(auto, 70%) auto;
+}
+
+.nge-chatbox-message-sender {
+  font-weight: bold;
+}
+
+.nge-chatbox-message-time {
+  text-align: right;
+}
+
+.nge-chatbox-message-content {
+  padding: 0.5em;
+  padding-top: 0.15em;
+}
+
+.nge-chatbox-sendmessage {
+  align-self: end;
+}
+
+/*.nge-chatbox-sendmessage > input {
+  color: #fff;
+  background-color: #000;
+}*/
 </style>
