d822015e265d7a79a4c5c4b4ba2fa6039cef273d
Chris Jordan <jordanchriss@gmail.com>
2020-02-12 15:53:43 -0500

    updated logged in user check for new middleauth schema

--- src/state.ts
+++ src/state.ts
@@ -213,7 +213,7 @@ class AppStore extends createModule({strict: false}) {
     const existingAuthURL = localStorage.getItem("auth_url");
 
     if (existingToken && existingAuthURL) {
-      let res = await authFetch(`https://${existingAuthURL}/user/me`);
+      let res = await authFetch(`https://${existingAuthURL}/api/v1/user/me`);
       let user = await res.json();
       let {name, email} = user;
       this.loggedInUser = {name, email};
