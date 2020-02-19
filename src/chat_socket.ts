0f49d549fcf5ebe5a0f60070796ec0d61004f8a5
Kai Kuehner <kvk@pobox.com>
2020-02-19 18:42:10 -0500

    connect to live chat server

--- src/chat_socket.ts
+++ src/chat_socket.ts
@@ -1,3 +1,3 @@
-const ws = new WebSocket('wss://api.pyrdev.eyewire.org:3000');
+const ws = new WebSocket('wss://pyrdev.eyewire.org/chat');
 //const ws = new WebSocket('ws://localhost:3000');
 export default ws;
