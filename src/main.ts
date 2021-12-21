08e90e0fec8f9b5fa097868a6f6ec570b12ff7a1
Oluwaseun Ogedengbe <seun40@gmail.com>
2021-12-21 14:30:53 -0500

    Seun bulbmenu (#57)
    * context menu bulb
    
    * multiple changes to changelog
    
    * simplify dataset

--- src/main.ts
+++ src/main.ts
@@ -118,33 +118,64 @@ function observeSegmentSelect(targetNode: Element) {
 
   // Options for the observer (which mutations to observe)
   const config = {childList: true, subtree: true};
-
+  const makeChangelogMenu =
+      (parent: HTMLElement, segmentIDString: string,
+       dataset: string): ContextMenu => {
+        const contextMenu = new ContextMenu(parent);
+        const menu = contextMenu.element;
+        menu.classList.add('neuroglancer-layer-group-viewer-context-menu');
+        const paramStr = `${segmentIDString}&dataset=${dataset}&submit=true`;
+        const host = 'https://prod.flywire-daf.com';
+        const menuOpt = [
+          ['Changelog', `${host}/progress/api/v1/query?rootid=${paramStr}`],
+        ];
+        if (dataset == 'fly_v31') {
+          menuOpt.push([
+            'Proofreading',
+            `${host}/neurons/api/v1/lookup_info?filter_by=root_id&filter_string=${
+                paramStr}`
+          ])
+        }
+        for (const [name, model] of menuOpt) {
+          // const widget = contextMenu.registerDisposer(new
+          // EnumSelectWidget(model));
+          const label = document.createElement('a');
+          label.style.display = 'flex';
+          label.style.flexDirection = 'row';
+          label.style.whiteSpace = 'nowrap';
+          label.textContent = name;
+          label.style.color = 'white';
+          label.href = model;
+          label.target = '_blank';
+          // console.log(model);
+          // label.appendChild(widget.element);
+          menu.appendChild(label);
+        }
+        return contextMenu;
+      };
   const createChangelogButton =
-      (segmentIDString: string, queryurl: string): HTMLButtonElement => {
+      (segmentIDString: string, dataset: DOMStringMap): HTMLButtonElement => {
         // Button for the user to copy a segment's ID
         const changelogButton = document.createElement('button');
         changelogButton.className = 'nge-segment-changelog-button';
         changelogButton.title =
             `Show changelog for Segment: ${segmentIDString}`;
         changelogButton.innerHTML = '💡';
-        changelogButton.addEventListener('click', async () => {
-          // changelogButton.disabled = true;
+        var cmenu = makeChangelogMenu(
+            changelogButton, segmentIDString, dataset.dataset!);
+        // console.log(cmenu);
+        changelogButton.addEventListener('click', (event: MouseEvent) => {
+          cmenu.show(event);
+        });
+        // console.log(queryurl);
+        /*changelogButton.addEventListener('click', async () => {
           const request =
               `${queryurl}${segmentIDString}/tabular_change_log?disp=True`;
 
           const params =
               `location=no,toolbar=no,menubar=no,width=620,left=0,top=0`;
           window.open(request, `Changelog for ${segmentIDString}`, params)
-          /* make fetch request
-          try {
-            const response = await fetch(request);
-            const body = await response.text();
-            changelogButton.disabled = false;
-          } catch (e) {
-            changelogButton.disabled = false;
-            throw e;
-          }*/
-        });
+        });*/
         return changelogButton;
       };
 
@@ -163,12 +194,8 @@ function observeSegmentSelect(targetNode: Element) {
         const segmentIDString =
             (<HTMLElement>item.querySelector('.segment-button')).dataset.segId!;
         if (!item.querySelector('.nge-segment-changelog-button')) {
-          item.appendChild(createChangelogButton(
-              segmentIDString,
-              `${
-                  new URL(item.dataset.source!)
-                      .origin}/segmentation/api/v1/table/${
-                  item.dataset.dataset}/root/`));
+          item.appendChild(
+              createChangelogButton(segmentIDString, item.dataset));
         }
       });
     }
@@ -202,6 +229,7 @@ function liveNeuroglancerInjection() {
 
 import {authTokenShared} from 'neuroglancer/authentication/frontend';
 import Config from './config';
+import {ContextMenu} from 'neuroglancer/ui/context_menu';
 
 class ExtendViewer extends Viewer {
   constructor(public display: DisplayContext) {
