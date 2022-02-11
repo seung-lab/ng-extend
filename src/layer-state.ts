8a1ce41546e6413e1d7ecf4a65f11d6ea6ab45fc
Kai Kuehner <kvk@pobox.com>
2022-02-11 15:47:23 -0500

    set brain mesh color to gray

--- src/layer-state.ts
+++ src/layer-state.ts
@@ -7,6 +7,7 @@ import {Uint64} from 'neuroglancer/util/uint64';
 import {config} from './main';
 import {CellDescription, ImageLayerDescription, SegmentationLayerDescription, Vector3} from './config';
 import {getLayerPanel, viewer} from './state';
+import {packColor} from 'neuroglancer/util/color';
 
 function getLayerByName(name: string) {
   if (!viewer) {
@@ -277,6 +278,8 @@ export class LayerState extends createModule
           if (layer instanceof SegmentationUserLayer) {
             layer.displayState.rootSegments.add(Uint64.ONE);
             layer.displayState.objectAlpha.value = config.brainMeshOpacity;
+            const segmentColor = new Uint64(packColor(vec3.fromValues(0.5, 0.5, 0.5)));
+            layer.displayState.segmentStatedColors.set(Uint64.ONE, segmentColor);
           }
         }
       });
