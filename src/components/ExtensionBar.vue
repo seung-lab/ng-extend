ed9a57862410b326152992f078da68f274b22143
Kai Kuehner <kvk@pobox.com>
2022-04-05 00:26:20 -0400

    only show checkout button in production

--- src/components/ExtensionBar.vue
+++ src/components/ExtensionBar.vue
@@ -31,8 +31,10 @@
     <div class="ng-extend-spacer"></div>
 
     <template v-if="appState.loadedViewer">
-      <button class="checkoutButton" @click="appState.checkoutNeuron()">Checkout</button>
-      <div class="ng-extend-spacer"></div>
+      <template v-if="inProduction">
+        <button class="checkoutButton" @click="appState.checkoutNeuron()">Checkout</button>
+        <div class="ng-extend-spacer"></div>
+      </template>
 
       <dropdown-list type="chooser" dropdown-group="extension-bar-right" id="imageLayerChooser" width="240px" hover="Choose image">
         <template #chooserTitle>
@@ -143,6 +145,9 @@ export default Vue.extend({
     },
     activeSegmentationLayer() {
       return layerProxy.activeSegmentationLayer;
+    },
+    inProduction() {
+      return layerProxy.activeSegmentationLayer && layerProxy.activeSegmentationLayer!.name!.startsWith("Production");
     }
   },
   methods: {
