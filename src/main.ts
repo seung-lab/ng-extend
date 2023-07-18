abc7eb6be9e47ea82d10320a03d7bb978335d363
Chris Jordan <jordanchriss@gmail.com>
2023-07-18 19:34:24 -0400

    added custom keybinds from spelunker, cleaned up viewer creation (custom changes no longer needed with new neuroglancer)

--- src/main.ts
+++ src/main.ts
@@ -4,65 +4,8 @@ import {createPinia} from 'pinia';
 import 'neuroglancer/ui/default_viewer.css';
 
 import App from 'components/App.vue';
-import {Viewer, ViewerOptions} from 'neuroglancer/viewer';
-import {setDefaultInputEventBindings} from 'neuroglancer/ui/default_input_event_bindings';
-import {bindTitle} from 'neuroglancer/ui/title';
-import {bindDefaultCopyHandler, bindDefaultPasteHandler} from 'neuroglancer/ui/default_clipboard_handling';
-import {DisplayContext} from 'neuroglancer/display_context';
-import {StatusMessage} from 'neuroglancer/status';
-import {makeDefaultViewer} from 'neuroglancer/ui/default_viewer';
-import {UrlHashBinding} from 'neuroglancer/ui/url_hash_binding';
 import {useLayersStore} from 'src/store';
-
-class ExtendViewer extends Viewer {
-  constructor(public display: DisplayContext) {
-    super(display, {
-      showLayerDialog: false,
-      showUIControls: true,
-    });
-  }
-}
-
-export function makeExtendViewer(options?: Partial<ViewerOptions>, target = document.getElementById('neuroglancer-container')! ) {
-  options;
-  try {
-    let display = new DisplayContext(target);
-    return new ExtendViewer(display);
-  } catch (error) {
-    StatusMessage.showMessage(`Error: ${error.message}`);
-    throw error;
-  }
-}
-
-declare var NEUROGLANCER_DEFAULT_STATE_FRAGMENT: string|undefined;
-
-function setupViewer() {
-  const viewer = (<any>window)['viewer'] = makeDefaultViewer();
-  setDefaultInputEventBindings(viewer.inputEventBindings);
-
-  const hashBinding = viewer.registerDisposer(
-      new UrlHashBinding(viewer.state, viewer.dataSourceProvider.credentialsManager, {
-        defaultFragment: typeof NEUROGLANCER_DEFAULT_STATE_FRAGMENT !== 'undefined' ?
-            NEUROGLANCER_DEFAULT_STATE_FRAGMENT :
-            undefined
-      }));
-  viewer.registerDisposer(hashBinding.parseError.changed.add(() => {
-    const {value} = hashBinding.parseError;
-    if (value !== undefined) {
-      const status = new StatusMessage();
-      status.setErrorMessage(`Error parsing state: ${value.message}`);
-      console.log('Error parsing state', value);
-    }
-    hashBinding.parseError;
-  }));
-  hashBinding.updateFromUrlHash();
-  viewer.registerDisposer(bindTitle(viewer.title));
-
-  bindDefaultCopyHandler(viewer);
-  bindDefaultPasteHandler(viewer);
-
-  return viewer;
-}
+import {setupDefaultViewer} from 'third_party/neuroglancer/ui/default_viewer_setup';
 
 function mergeTopBars() {
   const ngTopBar = document.querySelector('.neuroglancer-viewer')!.children[0];
@@ -79,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
     el.style.visibility = !!binding.value ? 'visible' : 'hidden';
   });
   app.mount('#app');
-  const viewer = setupViewer();
+  const viewer = setupDefaultViewer();
   initializeWithViewer(viewer);
   mergeTopBars();
 });
