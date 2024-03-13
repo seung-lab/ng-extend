cdab1a49a0567f89071ed61a3cf2187a4e1aab67
Alex Malakov <alexander.malakov@gmail.com>
2024-03-13 14:02:58 -0400

    since i've been mostly copying off lchtlab I mgiht as well swap to that branch and work backwards

--- src/main.ts
+++ src/main.ts
@@ -26,12 +26,110 @@ window.addEventListener('DOMContentLoaded', () => {
     el.style.visibility = !!binding.value ? 'visible' : 'hidden';
   });
   app.mount('#app');
-  const viewer = setupDefaultViewer();
+  // const viewer = setupDefaultViewer();
+  const viewer = setupViewer();
   initializeWithViewer(viewer);
   liveNeuroglancerInjection();
   mergeTopBars();
 });
 
+
+function setupViewer() {
+  const viewer = (<any>window)['viewer'] = makeExtendViewer();
+  setDefaultInputEventBindings(viewer.inputEventBindings);
+
+  // borrowed from setupDefaultViewer()
+  const bindNonLayerSpecificTool = (obj: unknown, toolKey: string, desiredLayerType: UserLayerConstructor, desiredProvider?: string) => {
+    let previousTool: Tool<Object>|undefined;
+    let previousLayer: UserLayer|undefined;
+    if (typeof obj === 'string') {
+      obj = {'type': obj};
+    }
+    verifyObject(obj);
+    const type = verifyObjectProperty(obj, 'type', verifyString);
+    viewer.bindAction(`tool-${type}`, () => {
+      const acceptableLayers = viewer.layerManager.managedLayers.filter((managedLayer) => {
+        const correctLayerType = managedLayer.layer instanceof desiredLayerType;
+        if (desiredProvider && correctLayerType) {
+          for (const dataSource of managedLayer.layer?.dataSources || []) {
+            const protocol = viewer.dataSourceProvider.getProvider(dataSource.spec.url)[2];
+            if (protocol === desiredProvider) {
+              return true;
+            }
+          }
+          return false;
+        } else {
+          return correctLayerType;
+        }
+      });
+      if (acceptableLayers.length > 0) {
+        const firstLayer = acceptableLayers[0].layer;
+        if (firstLayer) {
+          if (firstLayer !== previousLayer) {
+            previousTool = restoreTool(firstLayer, obj);
+            previousLayer = firstLayer;
+          }
+          if (previousTool) {
+            viewer.activateTool(toolKey, previousTool);
+          }
+        }
+      }
+    });
+  }
+
+  if (hasCustomBindings) {
+    for (const [key, val] of Object.entries(CUSTOM_BINDINGS!)) {
+      if (typeof val === 'string') {
+        viewer.inputEventBindings.global.set(key, val);
+      } else {
+        viewer.inputEventBindings.global.set(key, `tool-${val.tool}`);
+        const layerConstructor = layerTypes.get(val.layer);
+        if (layerConstructor) {
+          const toolKey = key.charAt(key.length - 1).toUpperCase();
+          bindNonLayerSpecificTool(val.tool, toolKey, layerConstructor, val.provider);
+        }
+      }
+    }
+  }
+
+  const hashBinding = viewer.registerDisposer(
+      new UrlHashBinding(viewer.state, viewer.dataSourceProvider.credentialsManager, {
+        defaultFragment: typeof NEUROGLANCER_DEFAULT_STATE_FRAGMENT !== 'undefined' ?
+            NEUROGLANCER_DEFAULT_STATE_FRAGMENT :
+            undefined
+      }));
+  viewer.registerDisposer(hashBinding.parseError.changed.add(() => {
+    const {value} = hashBinding.parseError;
+    if (value !== undefined) {
+      const status = new StatusMessage();
+      status.setErrorMessage(`Error parsing state: ${value.message}`);
+      console.log('Error parsing state', value);
+    }
+    hashBinding.parseError;
+  }));
+  hashBinding.updateFromUrlHash();
+  viewer.registerDisposer(bindTitle(viewer.title));
+
+  bindDefaultCopyHandler(viewer);
+  bindDefaultPasteHandler(viewer);
+
+  registerAnnotateCubeTool();
+
+  return viewer;
+}
+
+function makeExtendViewer() {
+  disableContextMenu();
+  disableWheel();
+  try {
+    let display =
+        new DisplayContext(document.getElementById('neuroglancer-container')!);
+    return new ExtendViewer(display);
+  } catch (error) {
+    StatusMessage.showMessage(`Error: ${error.message}`);
+    throw error;
+  }
+}
 
 
 
