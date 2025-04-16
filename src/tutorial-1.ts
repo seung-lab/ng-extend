923e8873a6a1c5fbf18ae0ea5ec330671d55afd6
Amy Robinson Sterling <amyleerobinson@gmail.com>
2025-04-16 13:26:40 -0400

    Update tutorial-1.ts
    ellipsoid annotation

--- src/tutorial-1.ts
+++ src/tutorial-1.ts
@@ -97,7 +97,7 @@ Don’t worry if you can’t find it - the Next button will take you there.`,
     image:
       "https://github.com/seung-lab/ng-extend/blob/cj-ca3-tutorial/src/images/inspector-nurro-2.png?raw=true",
     state:
-      "middleauth+https://global.daf-apis.com/nglstate/api/v1/5162363620687872",
+      "middleauth+https://global.daf-apis.com/nglstate/api/v1/4708711659470848",
   },
   //9 - new NG state middleauth+https://global.daf-apis.com/nglstate/api/v1/4893758698029056
   {
