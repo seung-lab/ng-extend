14f652cbfbf49fdaae019a3a763fedd6068335ef
celiad <celiacdavid@gmail.com>
2025-04-22 14:59:49 -0500

    Update tutorial-1.ts

--- src/tutorial-1.ts
+++ src/tutorial-1.ts
@@ -62,6 +62,8 @@ CTRL+V anywhere on the interface to PASTE in copied coordinates. This will jump 
       side: "bottom",
       offset: { x: 0, y: 0 },
     },
+    state:
+    "#!middleauth+https://global.daf-apis.com/nglstate/api/v1/5766780040511488",
   },
   //4 -- Using layers
   {
